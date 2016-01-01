0fa4a7a9db007e8340048e063fd9c426a31714e6
Beth Gilomen <gilomen2@gmail.com>
2016-01-01 17:10:49 -0600

    fixes #31 admins can view and delete all private wikis, even those they did not create

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -7,9 +7,23 @@ class Wiki < ActiveRecord::Base
 
   t = Wiki.arel_table
 
-  scope :visible_to, -> (user) { user ? Wiki.where(t[:private].eq(false).or(t[:user_id].eq(user.id))) : Wiki.where(t[:private].eq(false)) }
+  scope :visible_to, -> (user) {
+    if user.admin?
+      Wiki.all
+    elsif t[:user_id].eq(user.id)
+      Wiki.where(t[:private].eq(false).or(t[:user_id].eq(user.id)))
+    else
+      Wiki.where(t[:private].eq(false))
+    end
+  }
+
+
 
   scope :private_wikis, -> (user) { Wiki.where(t[:private].eq(true).and(t[:user_id].eq(user.id))) }
+
+  def private?
+    self.private == true
+  end
 
   private
     def set_public
