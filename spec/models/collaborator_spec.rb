10ebadcd44c8bc9e84d00b253e513763f4ca9997
Beth Gilomen <gilomen2@gmail.com>
2016-01-13 20:02:29 -0600

    Refactoring

--- spec/models/collaborator_spec.rb
+++ spec/models/collaborator_spec.rb
@@ -1,28 +1,27 @@
 require "rails_helper"
 
 RSpec.describe Collaborator, :type => :model do
+  before (:each) do
+    @user = User.new(email: 'someone@something.com', name: 'Bob', password: 'whatever')
+    @user.skip_confirmation!
+    @user.save!
+  end
+
   it 'can load the class' do
     expect(Collaborator).not_to be_nil
   end
 
   it 'can be created with a wiki and a user' do
-    user = User.new(email: 'someone@something.com', name: 'Bob', password: 'whatever')
-    user.skip_confirmation!
-    user.save!
     wiki = Wiki.create!(user_id: 999)
-
-    collaborator = Collaborator.new(wiki_id: wiki.id, user_id: user.id)
+    collaborator = Collaborator.new(wiki: wiki, user: @user)
 
     expect(collaborator).to be_valid
   end
 
   it 'is not valid when the user is the owner' do
-    user = User.new(email: 'someone@something.com', name: 'Bob', password: 'whatever')
-    user.skip_confirmation!
-    user.save!
-    wiki = Wiki.create!(user_id: user.id)
+    wiki = Wiki.create!(user: @user)
+    collaborator = Collaborator.new(wiki: wiki, user: @user)
 
-    collaborator = Collaborator.new(wiki_id: wiki.id, user_id: user.id)
     expect(collaborator).not_to be_valid
   end
 end
